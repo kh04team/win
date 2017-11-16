4da3f5f017558ccf799382e9be87200750445848
kh04team@gmail.com <kh04team@gmail.com>
2017-11-16 15:04:30 +0900

    장바구니 작업본

---
+++ 박종택/project_20171116/basket/goodsSettleForm.java
@@ -0,0 +1,163 @@
+package basket;
+
+import com.opensymphony.xwork2.ActionContext;
+import com.opensymphony.xwork2.ActionSupport;
+
+import goods.goods_bin;
+import goods.pagingAction;
+
+import com.ibatis.common.resources.Resources;
+import com.ibatis.sqlmap.client.SqlMapClient;
+import com.ibatis.sqlmap.client.SqlMapClientBuilder;
+
+import java.util.Map;
+import java.util.*;
+import java.io.Reader;
+import java.io.IOException;
+
+import java.net.*;
+
+public class goodsSettleForm extends ActionSupport {
+
+	public static Reader reader;
+	public static SqlMapClient sqlMapper;
+
+	private String id;
+
+	private List<basket_bin> list = new ArrayList<basket_bin>();
+	private basket_bin paramClass = new basket_bin();
+	private order_bin paramClass2 = new order_bin();
+	private basket_bin resultClass = new basket_bin();
+	private order_bin resultClass2 = new order_bin();
+	
+	private int currentPage = 1;
+	private int totalCount;
+	private int blockCount = 10;
+	private int blockPage = 5;
+	private String pagingHtml;
+	private pagingAction page;	
+	private int basket_cnt;
+	
+	public goodsSettleForm() throws IOException
+	{
+		reader = Resources.getResourceAsReader("sqlMapConfig.xml");
+		sqlMapper = SqlMapClientBuilder.buildSqlMapClient(reader);
+		reader.close();
+	}	
+	
+	public String execute() throws Exception {
+
+		setId((String)ActionContext.getContext().getSession().get("id"));
+		list = sqlMapper.queryForList("basket.selectOrderWaitInformAll", getId());
+		
+		int totalCount = list.size();
+		
+		setBasket_cnt(totalCount);
+		
+		int lastCount = totalCount;
+		
+		list = list.subList(0, lastCount);
+		
+		paramClass2 = new order_bin();
+		paramClass2.setId((String)ActionContext.getContext().getSession().get("id"));
+		
+		resultClass2 = (order_bin) sqlMapper.queryForObject("basket.selectOrderPriceSum", paramClass2);		
+		
+		return SUCCESS;
+	}
+
+	public String orderSettle() throws Exception {
+	
+		return SUCCESS;
+	}
+	
+	public int getBasket_cnt() {
+		return basket_cnt;
+	}
+
+	public void setBasket_cnt(int basket_cnt) {
+		this.basket_cnt = basket_cnt;
+	}
+	
+	public basket_bin getParamClass() {
+		return paramClass;
+	}
+
+	public void setParamClass(basket_bin paramClass) {
+		this.paramClass = paramClass;
+	}
+
+	public basket_bin getResultClass() {
+		return resultClass;
+	}
+
+	public void setResultClass(basket_bin resultClass) {
+		this.resultClass = resultClass;
+	}
+
+	public int getCurrentPage() {
+		return currentPage;
+	}
+
+	public void setCurrentPage(int currentPage) {
+		this.currentPage = currentPage;
+	}
+
+	public int getTotalCount() {
+		return totalCount;
+	}
+
+	public void setTotalCount(int totalCount) {
+		this.totalCount = totalCount;
+	}
+
+	public int getBlockCount() {
+		return blockCount;
+	}
+
+	public void setBlockCount(int blockCount) {
+		this.blockCount = blockCount;
+	}
+
+	public int getBlockPage() {
+		return blockPage;
+	}
+
+	public void setBlockPage(int blockPage) {
+		this.blockPage = blockPage;
+	}
+
+	public String getPagingHtml() {
+		return pagingHtml;
+	}
+
+	public void setPagingHtml(String pagingHtml) {
+		this.pagingHtml = pagingHtml;
+	}
+
+	public pagingAction getPage() {
+		return page;
+	}
+
+	public void setPage(pagingAction page) {
+		this.page = page;
+	}
+	
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}	
+	
+	public List<basket_bin> getList() {
+		return list;
+	}
+
+	public void setList(List<basket_bin> list) {
+		this.list = list;
+	}
+
+
+}
---
+++ 박종택/project_20171116/goods/basket_order_complete.jsp
@@ -0,0 +1,6 @@
+<%@ page language="java" contentType="text/html; charset=EUC-KR"
+    pageEncoding="EUC-KR"%>
+<script type="text/javascript">
+	alert("주문이 완료되었습니다.");
+	location.href = "/icase/orderlist.action";
+</script>
